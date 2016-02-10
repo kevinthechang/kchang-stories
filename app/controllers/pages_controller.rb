c761b2f5eb63f610bed9d7a56cb11496356c47aa
kevinthechang <kchang85@gmail.com>
2016-02-09 16:40:13 -0800

    added custom attribute update in pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,7 @@ class PagesController < ApplicationController
 	def about
 		intercom = Intercom::Client.new(app_id: 'umxbi8zj', api_key: '1da89d0c08354cc43301eca6bec0b25188903c41')
   		intercom.events.create(event_name: "Click About", created_at: Time.now.to_i, email: current_user.email)
-  		
+
 	end
 	def thanks
 	end
@@ -16,12 +16,13 @@ class PagesController < ApplicationController
 		intercom = Intercom::Client.new(app_id: 'umxbi8zj', api_key: '1da89d0c08354cc43301eca6bec0b25188903c41')
   		count = intercom.counts.for_type(type: 'user', count: 'segment')
   		@new_user_count = count.user["segment"][3]["New"]
+			intercom_custom_data.user[:rails_controller_attr] = "Test Rails Controller"
 
-  		
+
 	end
 	def message
 		intercom = Intercom::Client.new(app_id: 'umxbi8zj', api_key: '1da89d0c08354cc43301eca6bec0b25188903c41')
 		@usermessage = Message.new
-		
+
 	end
 end

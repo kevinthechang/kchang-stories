1003875d056df19aacf0e5058ae6ca3bf390c1d7
kevinthechang <kchang85@gmail.com>
2016-03-05 20:21:28 -0800

    updated webhook controller with rails caching

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,38 +1,42 @@
 class WebhookController < ApplicationController
-  # require 'OpenSSL'
   # before_filter :init_check_signature
-  #
-  # def check_signature
-  #   json_data = request.body.read
-  #   @request = json_data
-  #   if json_data != ""
-  #     secret = "kevinsecretshhhhh"
-  #     digest = OpenSSL::Digest.new('sha1')
-  #     # @@calculated_signature = OpenSSL::HMAC.hexdigest(digest, secret, json_data).prepend('sha1=')
-  #     # @@intercom_signature = request.env["HTTP_X_HUB_SIGNATURE"]
-  #     @@calculated_signature = "1234"
-  #     @@intercom_signature = "abcd"
-  #     @calculated_signature = @@calculated_signature
-  #     @intercom_signature = @@intercom_signature
-  #   else
-  #     if @@calculated_signature != nil
-  #       @calculated_signature = @@calculated_signature
-  #       @intercom_signature = @@intercom_signature
-  #     else
-  #       @calculated_signature = "Nothing"
-  #       @intercom_signature = "Received"
-  #     end
-  #   end
-  #   binding.pry
-  #   if @calculated_signature == @intercom_signature
-  #     @success = "Woohoo!"
-  #   else
-  #     @success = "Not the same!"
-  #   end
-  # end
-  #
+
+
+  def check_signature
+    # cache_signatures = ActiveSupport::Cache::MemoryStore.new
+    # cache_signatures.write('test','success')
+    json_data = request.body.read
+    @request = json_data
+    if json_data != ""
+      secret = "kevinsecretshhhhh"
+      digest = OpenSSL::Digest.new('sha1')
+      calculated_signature = OpenSSL::HMAC.hexdigest(digest, secret, json_data).prepend('sha1=')
+      intercom_signature = request.env["HTTP_X_HUB_SIGNATURE"]
+      # calculated_signature = "1234"
+      # intercom_signature = "abcd"
+      Rails.cache.write('calculated_signature',calculated_signature)
+      Rails.cache.write('intercom_signature',intercom_signature)
+      @calculated_signature = calculated_signature
+      @intercom_signature = intercom_signature
+    else
+      # binding.pry
+      if Rails.cache.read('calculated_signature') != nil
+        @calculated_signature = Rails.cache.read('calculated_signature')
+        @intercom_signature = Rails.cache.read('intercom_signature')
+      else
+        @calculated_signature = "Nothing"
+        @intercom_signature = "Received"
+      end
+    end
+    # binding.pry
+    if @calculated_signature == @intercom_signature
+      @success = "Woohoo!"
+    else
+      @success = "Not the same!"
+    end
+  end
+
   # def init_check_signature
-  #   @@calculated_signature ||= "Nothing"
-  #   @@intercom_signature ||= "Received"
+  #   cache_signatures = ActiveSupport::Cache::MemoryStore.new
   # end
 end

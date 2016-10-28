ce58631d0bfb4a2b527571ec65be2b5bb693822f
kevinthechang <kchang85@gmail.com>
2016-10-27 23:18:21 -0700

    add pry before webhook topic

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -27,6 +27,7 @@ class WebhookController < ApplicationController
       render json: { text: "Got it!", status: 200}
 
       # process conversation note and create user note
+      binding.pry
       parsed_json = JSON.parse(request.body.read)
       console.log(parsed_json.class)
       Rails.cache.write('webhook_topic', parsed_json.class)
